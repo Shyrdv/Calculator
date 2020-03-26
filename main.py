d498f17972f374e1166385751710915b9b2d5ff0
Daviid Valentino Bernth <davi9400@edu.eal.dk>
2020-03-26 12:21:20 +0100

    MySql test

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import kivy
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
+import mysql.connector
 
+db = mysql.connector.connect(
+    host="172.104.148.212",
+    user="daviid",
+    passwd="Ubuntob0I!",
+    database="Tfw",
+    auth_plugin="mysql_native_password"
+)
+mycursor = db.cursor()
+mycursor.execute("INSERT INTO Users (username, password, admin_rights) VALUES (%s,%s,%s)", ("Smolboi", "tinycock", True))
+db.commit()
 
 class CalcGridLayout(GridLayout):
 
